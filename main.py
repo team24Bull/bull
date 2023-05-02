b2de342256bbdf8f2f1aeb2d065b9860ce30268c
Ok Youngjin <oyj940331@gmail.com>
2023-05-02 14:23:06 +0900

    fix: remove try, except statement

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import random
 def create_num():
 	print("난이도 : easy(3), normal(4), hard(5)")
 	while True:
-		try:
-			digit = int(input("난이도를 입력하시오 : "))
-			if digit not in [3, 4, 5]:
-				print("3, 4, 5 중 난이도를 선택하세요.")
-			else:
-				break
-		except Exception as e:
-			print("3, 4, 5 중 난이도를 선택하세요.")
+		digit = input("난이도를 입력하시오 : ")
+		if digit not in ['3', '4', '5']:
+			print("난이도를 3, 4, 5 중에서 입력해주세요.")
+		else:
+			digit = int(digit)
+			break
 	nums = random.sample(range(10), k=digit)
 	return digit, nums
 

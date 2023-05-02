4a4f60e240e3f36f68afe3cbc2f2243ff15a92df
nayoung511 <nayoungbae511@gmail.com>
2023-05-02 14:38:59 +0900

    feat: 숫자 맞추기-strike기능 추가
    nayoung.py에 있던 숫자 맞추기- strike 기능을 main.py함수에 추가했습니다.

--- main.py
+++ main.py
@@ -13,5 +13,17 @@ def create_num():
 	return digit, nums
 
 
+# 숫자 맞추기
+def function3(comp, user, n):
+    # strike
+    strike = 0
+    for i in range (n):
+        # 값과 위치가 같다면
+        if comp[i] == user[i]:
+            # strike 값을 증가 시킨다
+            strike += 1
+
+    return strike
+
 if __name__ == '__main__':
 	print(create_num())

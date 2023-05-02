1ff96e65b06c95c804afeb6fb68a1ed35d970496
DKIMDK <imdeokey@gmail.com>
2023-05-02 14:53:42 +0900

    Feat: cow_count done
    인덱스를 비교하여 cows count를 출력합니다

--- cows.py
+++ cows.py
@@ -1,2 +1,6 @@
 def cows(user:list, answer:list):
     cow_count = 0
+    for i in user:
+        if i in answer and user.index(i) != answer.index(i):
+            cow_count += 1
+    return 'cows: ' + str(cow_count)

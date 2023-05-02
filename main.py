a7493eef0dd3ed9d88265d8f6f6522358c292ea0
Jung110 <aoemsltm119@gmail.com>
2023-05-02 17:38:59 +0900

    feat : Create play_game func
    모든 함수를 종합하여 게임 플레이 함수를 작성 하였습니다.

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import random
 
+
 def create_num():
-	print("난이도 : easy(3), normal(4), hard(5)")
-	while True:
-		digit = input("난이도를 입력하시오 : ")
-		if digit not in ['3', '4', '5']:
-			print("난이도를 3, 4, 5 중에서 입력해주세요.")
-		else:
-			digit = int(digit)
-			break
-	nums = random.sample(range(10), k=digit)
-	return digit, nums
+    '''
+    난이도 설정 및 랜덤으로 리스트 생성
+    '''
+    print("난이도 : easy(3), normal(4), hard(5)")
+    while True:
+        digit = input("난이도를 입력하시오 : ")
+        if digit not in ['3', '4', '5']:
+            print("난이도를 3, 4, 5 중에서 입력해주세요.")
+        else:
+            digit = int(digit)
+            break
+
+    nums = random.sample(range(10), k=digit)
+    return digit, nums
 
 def balls(user:list, answer:list):
+    '''
+    ball 여부 판단
+    '''
     ball_count = 0
     for i in user:
         if (i in answer) and (user.index(i) != answer.index(i)):
             ball_count += 1
     return  ball_count
 
-# 숫자 맞추기
+
 def get_strike_num(comp, user, n):
     '''
 	comp: 랜덤 숫자 리스트
@@ -29,21 +37,17 @@ def get_strike_num(comp, user, n):
     strike = 0
     for i in range (n):
         # 값과 위치가 같다면
-        if comp[i] == user[i]:
+        if comp[i] == int(user[i]):
             # strike 값을 증가 시킨다
             strike += 1
 
     return strike
 
-
-
-
-
 def bull_input(digits):	
-	print("정답으로 생각하는 값을 입력하세요.")
-	input_value = input()
-	# print(len(set(input_value)))
-	# print(type(input_value))
+	'''
+	사용자가 예상하는 숫자리스트 입력
+	'''
+	input_value = input('정답으로 생각하는 값을 입력하세요.: ')
 
 	try:
 		#정수검사
@@ -55,7 +59,7 @@ def bull_input(digits):
 		elif (len(input_value) != digits):
 			print("입력값의 자릿수가 다릅니다.")
 		else:
-			return int(input_value)			
+			return list(map(int,list(input_value)))		
 	except Exception as e:
 		print(e)
 		print("유효한 입력이 아닙니다")
@@ -66,10 +70,28 @@ def printOutFunc(user_num, nums):
     for i in user_num: ## user_nums의 숫자가 nums 안에 없으면 out을 세시오.
         if i not in nums :
             out_count += 1
-    return out_count
- 
+    return out_count 
+
+def play_game():
+    player_num = int(input("input player number :"))
+
+    for i in range(player_num):
+        player = input("input youre name: ")
+        diff,com_list = create_num()
+        count = 0
+        while True:
+            user_list = bull_input(diff)
+            strike_num = get_strike_num(com_list,user_list,diff) 
+            ball_num = balls(user_list,com_list)
+            out_num = printOutFunc(user_list , com_list)
+            print("strike : {} , ball : {} , out : {}".format(strike_num , ball_num , out_num))
+            count +=1
+            if strike_num == diff :
+                print('End turn player : {} , count : {}'.format(player,count))
+                break
+	
+        
+
 
 if __name__ == '__main__':
-	print(create_num())
-
->>>>>>> main
+	play_game()
